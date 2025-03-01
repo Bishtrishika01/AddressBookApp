0704799484fc3a0a6d8eb9fa6d4679209d7d1c63
Bishtrishika01 <bishtrishika66@gamil.com>
2025-03-01 22:56:53 +0530

    [Rishika] Add. Pom.xml

--- src/main/java/com/addressbook/addressbookapp/AddressBookAppApplication.java
+++ src/main/java/com/addressbook/addressbookapp/AddressBookAppApplication.java
@@ -1,14 +1,15 @@
 package com.addressbook.addressbookapp;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class AddressBookAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AddressBookAppApplication.class, args);
+		log.info("Address Book Application started successfully!");
 	}
-
 }
-
